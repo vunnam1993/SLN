671040d671b4c325879c72cb4da7b93d4620ff51
SLN <SLN@SLN-PC>
2019-09-13 23:18:02 -0700

    controller complete

--- SpringBoot/src/main/java/com/cts/bean/SpringBoot/Application.java
+++ SpringBoot/src/main/java/com/cts/bean/SpringBoot/Application.java
@@ -2,10 +2,12 @@ package com.cts.bean.SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.cts.bean.controllers")
 public class Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- SpringBoot/src/main/java/com/cts/bean/controllers/SampleController.java
+++ SpringBoot/src/main/java/com/cts/bean/controllers/SampleController.java
@@ -1,13 +1,13 @@
-package com.cts.controllers;
+package com.cts.bean.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path="/welcome")
 public class SampleController {
 	
-	public static String welcomeController() {
+	@RequestMapping("/welcome")
+	public String welcomeController() {
 		return "Welcome to Spring Boot";
 	}
 
