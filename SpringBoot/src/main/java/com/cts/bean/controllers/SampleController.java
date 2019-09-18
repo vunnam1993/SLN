731c7aee2b572aa139946110758a146c80bbeede
vunna <vunna@DESKTOP-D2TQ2ET>
2019-09-18 13:18:37 +0530

    Controller

--- SpringBoot/src/main/java/com/cts/bean/controllers/SampleController.java
+++ SpringBoot/src/main/java/com/cts/bean/controllers/SampleController.java
@@ -1,7 +1,14 @@
 package com.cts.bean.controllers;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+
+import com.cts.bean.beans.Student;
+import com.cts.bean.beans.StudentReplay;
 
 @RestController
 public class SampleController {
@@ -10,5 +17,22 @@ public class SampleController {
 	public String welcomeController() {
 		return "Welcome to Spring Boot";
 	}
+	
+	@RequestMapping(method=RequestMethod.POST ,value="/home")
+	public StudentReplay getDetails(@RequestBody Student student) {
+		
+		StudentReplay studentReplay= new StudentReplay();
+		studentReplay.setIds(student.getId());
+		studentReplay.setName(student.getStudentName());
+		studentReplay.setReplayAdress(student.getAddress());
+		studentReplay.setStatus("conformation Status");
+		
+		return studentReplay;
+		
+		
+	}
+	
+	
+	
 
 }
